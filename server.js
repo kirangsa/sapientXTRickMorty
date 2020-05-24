c1377eaa9ddecba76dc3c71437f28b2be3822576
Kiran <kiran.gsa@gmail.com>
2020-05-24 21:58:26 +0530

    fixing build issue

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require('express');
 
 const app = express();
 
-app.use(express.static('./dist/rickMorty'));
+app.use(express.static('./dist/rick-morty'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/rickMorty/'}),
+    res.sendFile('index.html', {root: 'dist/rick-morty/'}),
 );
 
 app.listen(process.env.PORT || 8080);
